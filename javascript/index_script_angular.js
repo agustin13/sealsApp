773a98eeed25769b6c0be554c2772daf1b28518b
Agustin Jara <agustin.rios136@gmail.com>
2017-03-16 19:53:53 -0700

    my app

--- javascript/index_script_angular.js
+++ javascript/index_script_angular.js
@@ -1,8 +1,19 @@
-var app = angular.module("myApp" , []);
-
-
 app.controller("formController", function($scope) {
 
+    $scope.notes = [];
+    note_counter = 0;
+
+    $scope.showNote = function() {
+        console.log($scope.entry_title);
+        console.log($scope.entry_desc);
+    };
 
 
-});
+    $scope.addNote = function() {
+        $scope.notes.push({title: $scope.entry_title, desc: $scope.entry_desc });
+        note_counter += 1;
+
+
+    };
+
+}
